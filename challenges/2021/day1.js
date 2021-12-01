b19d15ade3cf85f1c7b19981a42cfc9579c1013d
Joey Comhaire <joey.comhaire@gmail.com>
2021-12-01 06:39:08 +0100

    day 1

--- challenges/2021/day1.js
+++ challenges/2021/day1.js
@@ -1,13 +1,29 @@
 function A(input) {
-  return 0;
+  let count = 0;
+
+  for (let i = 1; i < input.length; i++) {
+    if (input[i] > input[i - 1]) {
+      count++;
+    }
+  }
+  return count;
 }
 
 function B(input) {
-  return 0;
+  let count = 0;
+  let lastSum = 0;
+  for (let i = 0; i < input.length - 2; i++) {
+    const sum = input[i] + input[i + 1] + input[i + 2];
+    if (sum > lastSum) {
+      count++;
+    }
+    lastSum = sum;
+  }
+  return count - 1; // first comparison doesnt count
 }
 
 function parse(input) {
-  return input;
+  return input.map((x) => parseInt(x, 10));
 }
 
 module.exports = { A, B, parse };
